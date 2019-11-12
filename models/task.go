d926b5bfc9970e4146c3ae3a92c64f7253806a6f
NaXieNianCaiGuoDeKang <472597709@qq.com>
2019-11-12 11:19:13 +0800

    update

--- models/task.go
+++ models/task.go
@@ -12,11 +12,11 @@ type Task struct {
 	TaskCreatorId  string          `json:"task_creator_id"`             // 任务创建人
 	TaskNumber     int             `json:"task_number"`                 // 任务编码
 	TaskName       string          `json:"task_name"`                   // 任务名称
-	TaskPriority   *int             `json:"task_priority"gorm:"default"` // 任务优先级
-	TaskType       *int             `json:"task_type"`                   // 任务类型
+	TaskPriority   *int            `json:"task_priority"gorm:"default"` // 任务优先级
+	TaskType       *int            `json:"task_type"`                   // 任务类型
 	TaskContent    string          `json:"task_content"`                // 任务内容
-	TaskSchedule   *int             `json:"task_schedule"gorm:"default"` // 任务进度
-	TaskStatus     *int             `json:"task_status"gorm:"default"`   // 任务状态
+	TaskSchedule   *int            `json:"task_schedule"gorm:"default"` // 任务进度
+	TaskStatus     *int            `json:"task_status"gorm:"default"`   // 任务状态
 	StartTime      *utils.JSONDate `json:"start_time"gorm:"default"`    // 任务开始时间
 	EndTime        *utils.JSONDate `json:"end_time"`                    // 任务预计完成时间
 	CreateTime     utils.JSONTime  `json:"create_time"`                 // 创建时间
@@ -26,8 +26,7 @@ type Task struct {
 
 type ChildTask struct {
 	Task
-	TaskFinisher  string `json:"task_finisher"`
-
+	TaskFinisher string `json:"task_finisher"`
 }
 
 // 根据任务ID查询子任务列表
@@ -58,9 +57,9 @@ type QueryTaskInfo struct {
 func FindTaskInfo(taskId string) (*QueryTaskInfo, error) {
 	var info QueryTaskInfo
 
-	err := Orm.Model(&Task{}).Select("task.*," +
-		"creator.name as task_creator, creator.avatar_url as creator_avatar," +
-		"finisher.name as task_finisher, father.task_id as  father_task_id," +
+	err := Orm.Model(&Task{}).Select("task.*,"+
+		"creator.name as task_creator, creator.avatar_url as creator_avatar,"+
+		"finisher.name as task_finisher, father.task_id as  father_task_id,"+
 		"father.task_number as father_task_number,father.task_name as father_task_name").
 		Joins("left join task as father on father.task_id = task.task_finisher_id").
 		Joins("inner join user as creator on creator.user_id=task.task_creator_id").
